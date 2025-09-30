ae852a5c5bc434aadd527efb35328e12a7092607
Rafal Niestrzeba <beast.art.rn@gmail.com>
2025-09-30 23:44:09 +0200

    feat(accordion): finalize initAccordion; toggle current and close previous

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,65 @@
 {
   'use strict';
 
-  // Product class: minimal constructor that only logs the created instance
+  // Product class
   class Product {
-    constructor() {
+    constructor(id, data) {
       const thisProduct = this;
+
+      // Save constructor arguments on the instance
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      // Render product in the menu (creates thisProduct.element)
+      thisProduct.renderInMenu();
+
+      // Initialize accordion behavior right after rendering
+      thisProduct.initAccordion();
+
       console.log('new Product:', thisProduct);
+    }
+
+    // Will render this product into the menu
+    renderInMenu() {
+      const thisProduct = this;
+
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      /* create element using utils.createDOMFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* append element to menu */
+      menuContainer.appendChild(thisProduct.element);
+    }
+
+    // Sets up accordion
+    initAccordion() {
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      if (!clickableTrigger) return;
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function (event) {
+        /* prevent default action for event */
+        event.preventDefault();
+
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct && activeProduct !== thisProduct.element) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
     }
   }
 
@@ -81,7 +135,7 @@
     initMenu: function () {
       const thisApp = this;
       for (let productId in thisApp.data.products) {
-        new Product(); // create instance without saving a reference
+        new Product(productId, thisApp.data.products[productId]);
       }
     },
 
